30e644829a5f6910cf098d172d8288d8621bd7f0
victorhugoRUB <victor.santos@rubcube.com>
2025-06-18 23:38:47 -0300

    feat: add static file serving for public directory and create initial index.html

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { join } from 'path';
+import * as express from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use('/site', express.static(join(__dirname, '..', 'public')));
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
